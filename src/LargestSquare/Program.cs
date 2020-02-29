53fbab43b30e3bad310b312abb0a54394113e79f
Bert Temme <btemme@hotmail.com>
2020-02-29 19:33:20 +0100

    fix sample prog

--- src/LargestSquare/Program.cs
+++ src/LargestSquare/Program.cs
@@ -1,5 +1,6 @@
 ﻿using LargestSquare.Core;
 using System;
+using System.Collections.Generic;
 using Tiles.Tools;
 
 namespace LargestSquare
@@ -13,9 +14,9 @@ namespace LargestSquare
             var p2 = new Tile { X = 1, Y = 1, Z=10 };
             var p3 = new Tile { X = 0, Y = 1, Z=10 };
 
-            var tiles = new Tiles() { p0, p1, p2, p3 };
+            var tiles = new List<Tile>() { p0, p1, p2, p3 };
 
-            var max2 = Calc.GetLargestSquare(tiles);
+            var max2 = LargestSquareCalculator.Calculate(tiles);
             Console.WriteLine(max2);
 
             Console.ReadKey();
